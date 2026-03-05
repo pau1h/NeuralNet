f0ed7666478c03edfd9bfc0c0d8b021f2cc181b8
pau1h <hawkinsanthonypaul@gmail.com>
2026-03-04 23:22:41 -0500

    added plots, tuned hyperparameters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from array import array
 from os.path  import join
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
@@ -15,8 +16,10 @@ y_train = train[:, 0] #n x 1
 x_test = test[:,1:]/255
 y_test = test[:,0]
 in_dim, H, out_dim, N = len(train[0]) - 1, 256, 10, len(train)
-w1 = np.random.randn(in_dim, H) * np.sqrt(2/in_dim) #w1 connecting all input vectors to all hidden layers. so this is 784 x 50. w1 = array of weights where w1[0][0] is the weight from the first input to the first hidden layer node
+w1 = np.random.randn(in_dim, H) * np.sqrt(2/in_dim) #w1 connecting all input vectors to all hidden layers. so this is 784 x H. w1 = array of weights where w1[0][0] is the weight from the first input to the first hidden layer node
 w2 = np.random.randn(H, out_dim) * np.sqrt(2/H) #using kaiming initialization
+loss_history = []
+acc_history = []
 
 
 def relu(x): return np.maximum(0, x)
@@ -37,21 +40,24 @@ def cross_entropy_loss(logits, y_true):
 
 def start_training(learning_rate, epochs, w1, w2):
     for epoch in range(epochs):
-        #we need to calculate the result of the hidden layer, h is supposed to be x . w1. x = 60000 x 784, w = 784 x 50, so h should be 60000 x 50
+        #we need to calculate the result of the hidden layer, h is supposed to be x . w1. x = 60000 x 784, w = 784 x H, so h should be 60000 x H
         z = x_train@w1
         h = relu(z) 
         logits = h@w2 #60000 x 10. We dont apply relu here because we want the logits for softmax
         preds = np.argmax(logits, axis=1)
         train_accuracy = np.mean(y_train == preds)
         L = cross_entropy_loss(logits, y_train)
-        print(f'Training accuracy: Epoch {epoch}, {train_accuracy*100}%, Loss: {np.mean(L)}')
+        mean_L = np.mean(L)
+        print(f'Training accuracy: Epoch {epoch}, {train_accuracy*100}%, Loss: {mean_L}')
+        loss_history.append(mean_L)
+        acc_history.append(train_accuracy)
         #now we need to calculate dloss / dlogits. this is 1/N * (softmax probabilities - y_onehot)
         probs = softmax(logits)
         y_onehot = np.zeros_like(logits)
         y_onehot[np.arange(N), y_train] = 1 #1 for correct label, 0 for everything else
-        dlogits = (1/N)*(probs - y_onehot) #60000 x 10, w2 is 50 x 10
-        dw2 = h.T@dlogits #50x10
-        dh = dlogits@w2.T #60000 x 50
+        dlogits = (1/N)*(probs - y_onehot) #60000 x 10, w2 is H x 10
+        dw2 = h.T@dlogits #Hx10
+        dh = dlogits@w2.T #60000 x H
         dz = dh * (z>0) #propagating through relu
         dw1 = x_train.T@dz
         #update weights
@@ -69,6 +75,17 @@ def main():
     preds = np.argmax(logits, axis=1)
     test_accuracy = np.mean(preds == y_test)
     print(f"Test accuracy: {test_accuracy*100}")
+    #plotting loss and accuracy 
+    plt.plot(acc_history)
+    plt.xlabel("Epoch")
+    plt.ylabel("Training Accuracy")
+    plt.title("Training Accuracy vs Epoch")
+    plt.show()
+    plt.plot(loss_history)
+    plt.xlabel("Epoch")
+    plt.ylabel("Cross Entropy Loss")
+    plt.title("Training Loss vs Epoch")
+    plt.show()
 
 
 
